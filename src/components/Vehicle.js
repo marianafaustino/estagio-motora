03b8c72a2ea8e9201f524a87a03e88c11532b8a9
marianafaustino <marifaustino@gmail.com>
2024-12-11 15:11:09 -0300

    feat: adiciona lógica para criar novo veículo e listar

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -1,7 +1,12 @@
+import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
 import React, { useEffect, useState } from 'react';
+import Footer from './Footer';
+import FloatButton from './FloatButton';
+import ModalAdd from './ModalAdd';
 
-const Vehicle = () => {
+const Vehicle = (stylevh) => {
     const [vehicles, setVehicles] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
         const fetchVehicles = async () => {
@@ -17,8 +22,36 @@ const Vehicle = () => {
         fetchVehicles();
     }, []);
 
+    const handleAddVehicle = async (event) => {
+        event.preventDefault(); 
+
+        const formData = new FormData(event.target);
+        const newVehicle = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch('http://localhost:3000/vehicles', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newVehicle),
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao adicionar veículo');
+            }
+
+            const addedVehicle = await response.json();
+            setVehicles((prevVehicles) => [...prevVehicles, addedVehicle]); 
+            setIsModalOpen(false); 
+        } catch (error) {
+            console.error('Erro ao adicionar veículo:', error);
+        }
+    };
+
+
     return (
-        <div className="relative overflow-x-auto">
+        <div className="relative overflow-x-auto" style={{height: "70vh"}}>
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
@@ -28,6 +61,8 @@ const Vehicle = () => {
                         <th scope="col" className="px-6 py-3">Longitude</th>
                         <th scope="col" className="px-6 py-3">Velocidade</th>
                         <th scope="col" className="px-6 py-3">Status</th>
+                        <th scope="col" className="px-6 py-3"></th>
+                        <th scope="col" className="px-6 py-3"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -39,10 +74,27 @@ const Vehicle = () => {
                             <td className="px-6 py-4">{vehicle.lng}</td>
                             <td className="px-6 py-4">{vehicle.speed}</td>
                             <td className="px-6 py-4">{vehicle.status === 'stopped' ? 'PARADO' : vehicle.status === 'moving' ? 'EM MOVIMENTO' : vehicle.status}</td>
+                            <td className="px-6 py-4">{<PencilSquareIcon/>}</td>
+                            <td className="px-6 py-4">{<TrashIcon/>}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <Footer>
+                <FloatButton disabled={false} onClick={() => setIsModalOpen(true)}/>
+            </Footer>
+            <ModalAdd isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <h2 className="text-lg font-bold mb-4 text-motoraDarkBlue">Adicionar Novo Veículo</h2>
+                <form onSubmit={handleAddVehicle}>
+                    <div className="mb-4">
+                        <label htmlFor="plate" className="block text-sm font-medium text-motoraDarkBlue">Placa</label>
+                        <input type="text" id="plate" name="plate" required className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"/>
+                        <label htmlFor="type" className="block text-sm font-medium text-motoraDarkBlue">Tipo</label>
+                        <input type="text" id="type" name="type" required className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"/>
+                    </div>
+                    <button type="submit" className="mt-2 w-full bg-motoraDarkBlue text-white rounded-md p-2 hover:bg-motoraLightBlue">Adicionar</button>
+                </form>
+            </ModalAdd>
         </div>
     );
 };
