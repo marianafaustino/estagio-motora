237377fe4fa669b52f30b28bb71e823b2f73300a
marianafaustino <marifaustino@gmail.com>
2024-12-16 15:27:40 -0300

    style: ajusta posição do xmarkicon

--- src/components/ModalAdd.js
+++ src/components/ModalAdd.js
@@ -6,9 +6,9 @@ const ModalAdd = ({ isOpen, onClose, children }) => {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="bg-white rounded-lg shadow-lg z-10 p-6">
+            <div className="bg-white rounded-lg shadow-lg z-10 p-6 relative">
                 <button className="absolute top-2 right-2" onClick={onClose}>
-                    {<XMarkIcon className="h-10 w-10 text-motoraDarkBlue hover:text-motoraLightBlue"/>}
+                    <XMarkIcon className="h-5 w-5 text-motoraDarkBlue hover:text-motoraLightBlue"/>
                 </button>
                 {children}
             </div>
